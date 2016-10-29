8c57cccb25e8d62401925817575ee1cf8c5f1cf4
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2016-10-29 21:52:46 -0200

    adiciona todos módulos necessários

--- app/scripts/app-modules.js
+++ app/scripts/app-modules.js
@@ -21,10 +21,18 @@
             'ui.bootstrap',
             'com.github.marcelothebuilder.housemm.home',
             'com.github.marcelothebuilder.housemm.clientes',
+            'com.github.marcelothebuilder.housemm.empresas',
+            'com.github.marcelothebuilder.housemm.entregadores',
+            'com.github.marcelothebuilder.housemm.pedidos',
+            'com.github.marcelothebuilder.housemm.produtos',
             'com.github.marcelothebuilder.housemm.datetime'
         ]);
 
     angular.module('com.github.marcelothebuilder.housemm.clientes', []);
+    angular.module('com.github.marcelothebuilder.housemm.empresas', []);
+    angular.module('com.github.marcelothebuilder.housemm.entregadores', []);
+    angular.module('com.github.marcelothebuilder.housemm.pedidos', []);
+    angular.module('com.github.marcelothebuilder.housemm.produtos', []);
     angular.module('com.github.marcelothebuilder.housemm.home', []);
     angular.module('com.github.marcelothebuilder.housemm.datetime', ['ui.bootstrap']);
 }());
