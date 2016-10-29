d18878f365ee727927a634529d3b5b2d9ef3913c
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2016-10-29 19:23:50 -0200

    commit inicial, gerado a partir de yeoman angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name unoparhousemmApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the unoparhousemmApp
+ */
+angular.module('unoparhousemmApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
